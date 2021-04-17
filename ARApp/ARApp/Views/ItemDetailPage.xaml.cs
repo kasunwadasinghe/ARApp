dc9808ca8553b6eecbdec830812f87d46f48dfb5
Kasun Nirmal Wadasinghe <kasun.wadasinghe@gmail.com>
2021-04-17 12:42:01 +0530

    Add project files.

---
+++ ARApp/ARApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ARApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ARApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ ARNugetPackage/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ARNugetPackage
+{
+    public class Class1
+    {
+    }
+}
